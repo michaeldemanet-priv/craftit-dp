a4bcb3e4f0a2aa77363d01920d3d14606ad3f81b
Michael Demanet <michael.demanet@craft-it.io>
2026-09-25 17:41:27 +0200

    resolve 127.0.0.1 to host.docker.internal

--- providers/dp-rest/src/provider.py
+++ providers/dp-rest/src/provider.py
@@ -17,7 +17,7 @@ from urllib.request import urlopen
 
 import riskfoundry as rf
 
-_DEFAULT_BASE_URL = "http://127.0.0.1:8110"
+_DEFAULT_BASE_URL = "http://host.docker.internal:8110"
 _TIMEOUT_SECONDS = 30
 
 
